b4bef6c4b989cc159155a1f6c99e26b683323052
Sanjaya-BahadurRai <104250072+Sanjaya-BahadurRai@users.noreply.github.com>
2024-06-27 08:45:31 +0545

    insert data in table and find prime number

--- loop.php
+++ loop.php
@@ -7,16 +7,16 @@
 
 <h3>While loop</h3>
 <?php
-    $i = 1;
-    while($i<=10){
-        echo "\t".$i;
-        $i++;
+    $num = 1;
+    while($num<=10){
+        echo "\t".$num;
+        $num++;
     }
     echo "<br>";
-    $i = 10;
-    while($i>0){
-        echo "\t".$i;
-        $i--;
+    $num = 10;
+    while($num>0){
+        echo "\t".$num;
+        $num--;
     }
 ?>
 <h3>For loop</h3>
@@ -29,7 +29,7 @@ echo "<br>";
     for($j=1;$j<=50;$j++)
         if($j%2==0)
             echo "\t" .$j;
-//Find the prime number : homework
+
     for($j=1;$j<=50;$j++)
         if($j%2==0)
             echo "\t" .$j;
@@ -91,3 +91,96 @@ foreach($group_of_students as $std){
     echo "<br>";
 }
 ?>
+
+
+<h3>Homework 1: Display the prime number between two given number.</h3>
+<?php
+    $a= 5;
+    $b=30;
+    $n;
+    for($num=$a;$num<=$b;$num++){
+        $isprime = 1;
+
+        //checks if num is a prime
+        if($num<=1){
+            $isprime = 0;
+        }
+        else{
+            for($i=2;$i*$i<=$num;$i++){
+                if($num%$i==0){
+                    $isprime = 0;
+                    break;
+                }
+            }
+        }
+        if($isprime){
+            echo "<br> . $num";
+        }
+    }
+?>
+
+<h3>Homework 1: check if the given number is prime number or not.</h3>
+<?php
+    $num = 53;
+    $isprime = 1;
+        //checks if num is a prime
+        if($num == 0 && $num == 1){
+            $isprime = 0;
+        }
+        
+        for($i=2;$i*$i<=$num;$i++){
+            if($num%$i==0){
+                $isprime = 0;
+                break;  
+            }
+            
+        }
+        if($isprime){
+            echo "The given number is a prime number";
+        }
+        else{
+            echo "The given number is not a prime number";
+        }
+        
+?>
+
+
+<h3>Display associative array using foreach loop in table</h3>
+<?php
+$group_of_students = [
+    [
+        "id" => "A01",
+        "name" => "ram",
+        "email" => "ram@gmail.com"
+    ],
+    [
+        "id" => "A02",
+        "name" => "shyam",
+        "email" => "ram@gmail.com"
+    ],
+    [
+        "id" => "A03",
+        "name" => "hari",
+        "email" => "ram@gmail.com"
+    ]
+];
+?>
+
+<table border = "1">
+    <thead>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Email</th>
+    </thead>
+        <?php foreach($group_of_students as $std){ ?>
+        <tr>
+            <td><?php echo $std ['id'];?> </td>
+            <td><?php echo $std ['name'];?> </td>
+            <td><?php echo $std ['email'];?> </td>
+        </tr>
+        <?php } ?>
+    <tbody>
+    </tbody>
+</table>
+
+
